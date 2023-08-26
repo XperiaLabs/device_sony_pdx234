89ca6bf46a055e4755f68641487096b919e416bb
Luofan Chen <clfbbn@gmail.com>
2023-08-27 00:53:15 +0800

    pdx234: Set soong config for touch hal
    Change-Id: Ia34d6f9eab25b8fd47d5fe28c4bef0b8a71c5dd4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -242,6 +242,10 @@ DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml \
     $(DEVICE_PATH)/configs/vintf/network_manifest.xml
 
+# Lineage Touch HAL
+$(call soong_config_set,sony_touch,board,yodo)
+$(call soong_config_set,sony_touch,touch_ctrl_path,/sys/devices/virtual/input/lxs_ts_input)
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
